25608bb0a852f45a57d1ae22224d83021d7e07de
Caryl Henry <carolynhenry2021@u.northwestern.edu>
2020-01-10 16:35:02 -0600

    Modified GET request for paths and checks for redirect response codes

--- networking1.py
+++ networking1.py
@@ -1,9 +1,9 @@
 import socket
 
 address = "insecure.stevetarzia.com"
+path = "/basic.html"
 remote_ip = socket.gethostbyname(address)
-# bad request
-request = "GET / HTTP/1.0\nHost: " + address + "\n\n"
+request = "GET " + path + " HTTP/1.0\nHost: " + address + "\n\n"
 
 # set up a client socket and send an HTTP GET request
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,6 +12,16 @@ client.send(bytes(request, 'utf-8'))
 
 # when recv returns 0 bytes, the server has closed the connection
 result = client.recv(1000)
+print('first result:')
+print(result)
+print(result[9:12])
+response_code = result[9:12]
+if response_code == b'301':
+    print('301 Response detected')
+elif response_code == b'302':
+    print('302 response detected')
+else:
+    print('not a 301 or 302 response')
 while(len(result) > 0):
     result = client.recv(1000)
     print(result)
