57913dafba20bc22609d1d9a9a1c8e93b82edbcd
Caryl Henry <carolynhenry2021@u.northwestern.edu>
2020-01-22 16:47:52 -0600

    Handles +inf and -inf args, overflow products, and JSON has inf args and products as strings

--- http_server3.py
+++ http_server3.py
@@ -21,52 +21,59 @@ while True:
     end_line = line.find('\r\n')
     line = line[:end_line]
 
-
     # Parse the request to get the operation
+    operation_start = line.find('/')+1
+    operation_end = line.find('?')
 
-    operation_location = line.find('/')+1
-    operation_right = line.find('?')
     # operation = line[operation_location: operation_location + 7]
-    operation = line[operation_location: operation_right]
+    operation = line[operation_start: operation_end]
     status_code = ''
     if operation != 'product':
         status_code = '404 Not Found'
 
     # Parse the rest of the request to get the operands
-    rest = line[operation_location + 8:-9]
-    values = rest.split('&')
-    operands = []
+    operand_string = line[operation_end+1:line.find(' ', operation_end)]
+    values = operand_string.split('&')
+    operands = {}
+    product = 1
     for variable in values:
         left = variable.find('=') + 1
         x = variable[left:]
         try:
-            x = float(x)
-        except:
-            print('')
-
-        if (not isinstance(x,float)) and not status_code:
+            number = float(x)
+            if x == 'inf' or x == '+inf' or x == '-inf':
+                operands[number] = x
+            else:
+                operands[number] = number
+        except ValueError:
             status_code = '400 Bad Request'
-        operands.append(x)
 
     if not status_code:
         status_code = '200 OK'
         # Do the math
         product = 1
-        for operand in operands:
-            product = operand * product
+        for operand in operands.keys():
+            try:
+                product *= operand
+                print(product)
+            except OverflowError:
+                if product > 0 and number > 0 or product < 0 and number < 0:
+                    product = float('+inf')
+                else:
+                    product = float('-inf')
+        if product == float('+inf'):
+            product = '+inf'
+        elif product == float('-inf'):
+            product = '-inf'
 
-        # Format into JSON
         response_body = json.dumps(
-            {'operation': 'product', 'operands': operands, 'result': product})
-        okay = 'HTTP/1.0 200 OK\r\n'
-        content_type = 'Content-Type: application/json\r\n\r\n'
-    
-        conn.send(okay.encode())
-        conn.send(content_type.encode())
+            {'operation': 'product', 'operands': list(operands.values()), 'result': product})
+        header = 'HTTP/1.0 200 OK\r\nContent-Type: application/json\r\n\r\n'
+
+        conn.send(header.encode())
         conn.send(response_body.encode())
     else:
         err = 'HTTP/1.0 ' + status_code + '\r\n'
         conn.send(err.encode())
 
     conn.close()
-
